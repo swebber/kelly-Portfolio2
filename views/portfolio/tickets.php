c0c2dcc4286a80628936b112e36032f61f2fd91d
swebber <scott@teagan.org>
2016-08-25 07:25:54 -0400

    finialized the tickets portfolio sub-page

--- views/portfolio/tickets.php
+++ views/portfolio/tickets.php
@@ -1,19 +1,42 @@
-<p>Tickets</p>
+<div class="portfolio-content">
 
-<ul id="bxslider">
-    <li><img src="/assets/img/tickets/slider-tickets-1.jpg" /></li>
-    <li><img src="/assets/img/tickets/slider-tickets-2.jpg" /></li>
-    <li><img src="/assets/img/tickets/slider-tickets-3.jpg" /></li>
-    <li><img src="/assets/img/tickets/slider-tickets-4.jpg" /></li>
-    <li><img src="/assets/img/tickets/slider-tickets-5.jpg" /></li>
-</ul>
+    <h1>Tickets</h1>
 
-<div id="bxslider-pager">
-    <li data-slideIndex="0"><a href=""><img src="/assets/img/tickets/thumb-tickets-1.jpg" /></a></li>
-    <li data-slideIndex="1"><a href=""><img src="/assets/img/tickets/thumb-tickets-2.jpg" /></a></li>
-    <li data-slideIndex="2"><a href=""><img src="/assets/img/tickets/thumb-tickets-3.jpg" /></a></li>
-    <li data-slideIndex="3"><a href=""><img src="/assets/img/tickets/thumb-tickets-4.jpg" /></a></li>
-    <li data-slideIndex="4"><a href=""><img src="/assets/img/tickets/thumb-tickets-5.jpg" /></a></li>
+    <ul id="bxslider">
+        <li><img src="/assets/img/tickets/slider-tickets-1.jpg" /></li>
+        <li><img src="/assets/img/tickets/slider-tickets-2.jpg" /></li>
+        <li><img src="/assets/img/tickets/slider-tickets-3.jpg" /></li>
+        <li><img src="/assets/img/tickets/slider-tickets-4.jpg" /></li>
+        <li><img src="/assets/img/tickets/slider-tickets-5.jpg" /></li>
+    </ul>
+
+    <div id="bxslider-pager">
+        <li data-slideIndex="0"><a href=""><img src="/assets/img/tickets/thumb-tickets-1.jpg" /></a></li>
+        <li data-slideIndex="1"><a href=""><img src="/assets/img/tickets/thumb-tickets-2.jpg" /></a></li>
+        <li data-slideIndex="2"><a href=""><img src="/assets/img/tickets/thumb-tickets-3.jpg" /></a></li>
+        <li data-slideIndex="3"><a href=""><img src="/assets/img/tickets/thumb-tickets-4.jpg" /></a></li>
+        <li data-slideIndex="4"><a href=""><img src="/assets/img/tickets/thumb-tickets-5.jpg" /></a></li>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h2>Take a ticket to travel the world.</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+            <p>This project was created from a prompt to create a set of unified cards containing only typography &mdash; I wanted to interprete cards into something that is meant to be more tactile. Tickets, have a strong sense of purpose and are an important object that can signify the quality of the experience a person has signed up and paid to do.</p>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 skils-list">
+            <h2>Tools</h2>
+            <ul>
+                <li>Illustrator</li>
+                <li>InDesign</li>
+            </ul>
+        </div>
+    </div>
+
 </div>
 
 <script>
